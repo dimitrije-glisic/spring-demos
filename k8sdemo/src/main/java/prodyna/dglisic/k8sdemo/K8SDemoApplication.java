82c66cfc68bf825ed8df4ac2bf242a5ff1a33b54
Dimitrije Glisic <52169396+dimitrije-glisic@users.noreply.github.com>
2022-07-28 15:55:24 +0200

    Add Readme and change endpoint path

--- k8sdemo/src/main/java/prodyna/dglisic/k8sdemo/K8SDemoApplication.java
+++ k8sdemo/src/main/java/prodyna/dglisic/k8sdemo/K8SDemoApplication.java
@@ -3,18 +3,20 @@ package prodyna.dglisic.k8sdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api")
 public class K8SDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(K8SDemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
